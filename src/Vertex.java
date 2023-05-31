75b49f6ba0478a681cca22e997612e7ad0d10035
AlikhanSagatbekov <221112@astana.edu.kz>
2023-05-31 23:51:50 +0600

    hasEdge method implementation

--- src/Vertex.java
+++ src/Vertex.java
@@ -3,29 +3,40 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Vertex<V>{
+public class Vertex<V> {
     private V data;
-        private Map<Vertex<V>, Double> adjacentVertices; // hashmap contains vertices as keys and weight as values
-    public Vertex(V data){ // constructor
-            this.data = data;
-            adjacentVertices = new HashMap<>();
-        }
-        public void addAdjacentVertex(Vertex<V> destination, double weight){ // to add new connection
-            adjacentVertices.put(destination, weight);
-        }
-    public void removeAdjacentVertex(Vertex<V> destination){
+    private Map<Vertex<V>, Double> adjacentVertices; // hashmap contains vertices as keys and weight as values
+
+    public Vertex(V data) { // constructor
+        this.data = data;
+        adjacentVertices = new HashMap<>();
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) { // to add new connection
+        adjacentVertices.put(destination, weight);
+    }
+
+    public void removeAdjacentVertex(Vertex<V> destination) {
         adjacentVertices.remove(destination);
     }
 
-    public V getData(){
-            return data;
-        }
+    public V getData() {
+        return data;
+    }
 
-        public List<Vertex<V>> getNeighbours(){ // to get all the vertices that are connected to this
-            ArrayList<Vertex<V>> neighbours = new ArrayList<>(); // vertices are stores in ArrayList before returning
-            for(Vertex<V> vertex: adjacentVertices.keySet()){
-                neighbours.add(vertex);
-            }
-            return neighbours;
+    public List<Vertex<V>> getNeighbours() { // to get all the vertices that are connected to this
+        ArrayList<Vertex<V>> neighbours = new ArrayList<>(); // vertices are stores in ArrayList before returning
+        for (Vertex<V> vertex : adjacentVertices.keySet()) {
+            neighbours.add(vertex);
+        }
+        return neighbours;
+    }
+
+    public boolean hasEdge(Vertex<V> destination) {
+        if (adjacentVertices.containsKey(destination)) {
+            return true;
+        } else {
+            return false;
         }
     }
+}
