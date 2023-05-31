a71aad4de0687df7eaec8fc1bf1da1040b1b5c4a
AlikhanSagatbekov <221112@astana.edu.kz>
2023-05-31 23:53:25 +0600

    BFS algo implementation

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,24 @@ public class Main {
     public static void main(String[] args) {
         MyGraph<Integer> graph = new MyGraph<>();
 
+        // Create vertices
+        graph.addVertex(0);
         graph.addVertex(1);
         graph.addVertex(2);
         graph.addVertex(3);
         graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
+        graph.addVertex(7);
 
-        graph.addEdge(1,2,10);
-        graph.addEdge(2,3,20);
-        graph.addEdge(3, 4, 30);
-        graph.addEdge(4,1,40);
-        graph.addEdge(3,2,50);
-        graph.addEdge(3,1,40);
+        graph.addEdge(0,1,10);
+        graph.addEdge(0,2,20);
+        graph.addEdge(0, 3, 30);
+        graph.addEdge(0,4,40);
+        graph.addEdge(1,5,50);
+        graph.addEdge(1,6,40);
+        graph.addEdge(2,7,40);
 
-        graph.removeEdge(1,4);
-        graph.printGraph();
+        graph.BFS(0);
     }
 }
